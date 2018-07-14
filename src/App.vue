bd74c90c2e7e6becedada39915e42236c71c5411
Calum Baines <calum.baines@hotmail.co.uk>
2018-07-14 10:58:22 +0100

    sass loader included for styling and initial chat component added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Chat />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Chat from './components/Chat/Chat.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Chat
   }
 }
 </script>
---
+++ src/components/Chat/Chat.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="Chat">
+      Chat area
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+  props: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+  // Leave import variables in here 
+  @import "../../assets/scss/main.scss";
+  // If creating a new component import component style sheet here as shown below 
+  @import "Chat.scss";
+</style>
